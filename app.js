7702a80cbba05aa432b7d8c23619640210fa0ae0
Samrat <bodasamrat3@gmail.com>
2021-11-16 07:01:26 +0530

    asxju

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
 const port = process.env.PORT || 8080; //
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = 'mongodb+srv://sammy:qweasdzxc@cluster1.vnh9c.mongodb.net/blogs?retryWrites=true&w=majority'; 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(port))
-  .catch(err => console.log(err));
+const dbURI =
+  "mongodb+srv://sammy:qweasdzxc@cluster1.vnh9c.mongodb.net/blogs?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
 // routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 // blog routes
-app.use('/blogs', blogRoutes);
+app.use("/blogs", blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
